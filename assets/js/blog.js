d6f21ccfb0bb3f99d94ffcdb8b064a48f14d5ff6
Knight <734648@DD>
2022-12-07 13:19:05 -0800

    brought in blog page from laptop

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,24 +1,86 @@
 // global variables
 const container = document.getElementById('#container');
+// post section
 const postSection = document.getElementById('#post-section');
-const articleTitle = document.querySelector('.article-title');
-const article = document.querySelector('.date');
-const articleDate = document.querySelector('.article-date');
+let articleTitle = document.querySelector('.article-title');
+let article = document.querySelector('.date');
+let articleDate = document.querySelector('.article-date');
 let articleTime = document.querySelector('.article-time');
-const articleContent = document.querySelector('.article-content');
+let articleContent = document.querySelector('.article-content');
+// btn controls
+const deleteCommentBtn = document.querySelector('.delete-comment');
+const editCommentBtn = document.querySelector('.edit-comment');
+const submitBtn = document.querySelector('.add-comment');
+const inputSection = document.querySelector('.input-section');
+// form error section
+const userNameEl = document.querySelector('#user-name-err');
+const userEmailEl = document.querySelector('#user-email-err');
+const userCommetEl = document.querySelector('#user-comment-err');
+const userWebsiteEl = document.querySelector('#user-website-err');
+const userImgEl = document.querySelector('#user-img-err');
+// comment section
+const commentArticle = document.querySelector('.comments');
+const postComment = document.querySelector('.post-comment');
+const clearBtn = document.querySelector('.clear-btn');
 const displayAmPm = true;
+const errors = {};
+let editElement;
+let editFlag = false;
+let editID = '';
 
+// form variables
+const userCommentForm = document.querySelector('.comment-form');
+const userWebsite = document.querySelector('.user-website');
+// const userCommentValues = JSON.parse(localStorage.getItem('userCommentValues')) || [];
+// console.log(userCommentValues);
 
-
-class Blog {
-constructor(id,title,date,time,content) {
-    this.id = id;
-    this.title = title;
-    this.date = date;
-    this.time = time;
-    this.content = content;
+// submit btn function
+function submitCommentForm(e){
+    e.preventDefault();
+    const userName = document.querySelector('.user-name').value;
+    const userEmail = document.querySelector('.user-email').value;
+    const userImg = document.querySelector('.user-img').value;
+    const userComment = document.querySelector('.user-comment').value;
+    const id = new Date().getTime();
+    const user = {
+        id: id,
+        userName: userName,
+        userEmail: userEmail,
+        userImg: userImg,
+        userComment: userComment,
+    }
+    
+    const userCommentValues = JSON.parse(localStorage.getItem('userCommentValues')) || [];
+    userCommentValues.push(user);
+    localStorage.setItem('userCommentValues', JSON.stringify(userCommentValues));
+    addUserComments(userCommentValues);
+    this.reset();
 }
+// console.log(userCommentValues);
+
+function addUserComments(comments = []){
+    
+    let displayCommentList = comments.map(function(comment){
+        return `
+        <div class="post-comment" id="id">
+        <img src="${comment.userImg}" alt="Photo of User" class="user-pic">
+        <h1 class="comment-title">Hello World</h1>
+        <h1 class="commentor-user-name">${comment.userName}</h1>
+        <h1 class="commentor-user-email">${comment.userEmail}</h1>
+        <p class="commentor-article">${comment.userComment}</p>
+        </div>
+        `
+    });
+    displayCommentList = displayCommentList.join('');
+    commentArticle.innerHTML = displayCommentList;
 }
+// event listener
+userCommentForm.addEventListener('submit', submitCommentForm);
+
+// run function()'s
+// addUserComments(userCommentValues);
+
+
 
 // function()'s
 function currentDate(){
@@ -26,13 +88,30 @@ function currentDate(){
     let month = today.getMonth();
     let date = today.getDate();
     let year = today.getFullYear();
-
-    articleDate.innerHTML = `${getMonthName(month)}&ensp;${date}<sub>th</sub>,&ensp;${year}`;
+    let suffix = getSuffix(date);
+    articleDate.innerHTML = `${getMonthName(month)}&ensp;${date}<sub>${suffix}</sub>,&ensp;${year}`;
 }
 
 function getMonthName(month) {
     const date = new Date().setMonth(month);
     return Intl.DateTimeFormat('en-US', { month: "long" }).format(date);
+}
+
+function getSuffix(dateNumber){
+    switch (dateNumber){
+        case 1:
+        case 21:
+        case 31:
+            return 'st';
+        case 2:
+        case 22:
+            return 'nd';
+        case 3:
+        case 23:
+            return 'rd';
+        default:
+            return 'th';
+    }
 }
 
 function currentTime(){
@@ -54,9 +133,5 @@ function zeroToTime(n){
     return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
-function getPost(){
-    
-}
 currentDate();
 currentTime();
-console.log(Blog.prototype);
