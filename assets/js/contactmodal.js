d62fe95f1fa4e38b35025397e3712c5b0d512e07
= <=>
2022-12-12 10:07:34 -0800

    working on building a new contact modal with js

--- assets/js/contactmodal.js
+++ assets/js/contactmodal.js
@@ -1,5 +1,14 @@
-const contactBtn = document.querySelector('.contact-btn'
-);
+// Get modal 
+const contactBtn = document.getElementById('contact-modal');
+// Get open modal btn
+const openContactModalBtn = document.getElementById('open-contact-modal-btn');
+// Get close modal btn
+const closeContactModalBtn = document.getElementById('close-contact-modal-btn');
+
+
+
+
+
 
 function submitContactForm(e){
   e.preventDefault();
