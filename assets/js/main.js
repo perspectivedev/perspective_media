d042b5863460eb2b2e6b1d185d0cbbf3b6bad9c1
= <=>
2022-12-13 12:53:43 -0800

    fixed the reset method for registration form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // global variables
 const container = document.querySelectorAll('#body-container');
-console.log(container);
+// console.log(container);
 const time = document.getElementById('current-time');
 const clientName = document.getElementById('client-name');
 const clientMessage = document.getElementById('client-message');
@@ -9,29 +9,29 @@ const displayAmPm = true;
 
 
 
-// landing page functions
-function currentTime(){
-    let today = new Date();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
+// // landing page functions
+// function currentTime(){
+//     let today = new Date();
+//     let hour = today.getHours();
+//     let min = today.getMinutes();
+//     let sec = today.getSeconds();
 
-    // Set AM or PM
-    const amPM = hour >= 12 ? 'PM' : 'AM';
+//     // Set AM or PM
+//     const amPM = hour >= 12 ? 'PM' : 'AM';
     
-    // 12hr format
-    hour = hour % 12 || 12;
+//     // 12hr format
+//     hour = hour % 12 || 12;
 
-    // Output time
-    time.innerHTML = `${hour}<span>:</span>${zeroToTime(min)}<span>:</span>${zeroToTime(sec)} ${displayAmPm ? amPM : ''}`;
+//     // Output time
+//     time.innerHTML = `${hour}<span>:</span>${zeroToTime(min)}<span>:</span>${zeroToTime(sec)} ${displayAmPm ? amPM : ''}`;
     
-    setTimeout(currentTime, 1000);
-}
+//     setTimeout(currentTime, 1000);
+// }
 
-// add zeros
-function zeroToTime(n){
-    return (parseInt(n, 10) < 10 ? '0' : '') + n;
-}
+// // add zeros
+// function zeroToTime(n){
+//     return (parseInt(n, 10) < 10 ? '0' : '') + n;
+// }
 
 // set background and greeting
 // function setBackgroundGreeting(){
@@ -54,61 +54,61 @@ function zeroToTime(n){
                 //         // greeting.textContent = 'Good Evening';
     //     }
     // }
-    currentTime();
+    // currentTime();
 
 
-    function getClientName(){
-        if(localStorage.getItem('name') === null){
-        clientName.textContent = `${'[Enter name].'}`;
-    } else {
-        clientName.textContent = localStorage.getItem('name');
-    }
-}
-// set client name
+//     function getClientName(){
+//         if(localStorage.getItem('name') === null){
+//         clientName.textContent = `${'[Enter name].'}`;
+//     } else {
+//         clientName.textContent = localStorage.getItem('name');
+//     }
+// }
+// // set client name
 
-function setClientName(e){
-    if(e.type === 'keypress'){
-        if(e.keyCode == 13){
-            localStorage.setItem('name', e.target.innerText);
-            clientName.blur();
-        }
-    }else{
-        localStorage.setItem('name', e.target.innerText);
-    }
-}
-console.log(setClientName.textContent);
-// Get client message 
+// function setClientName(e){
+//     if(e.type === 'keypress'){
+//         if(e.keyCode == 13){
+//             localStorage.setItem('name', e.target.innerText);
+//             clientName.blur();
+//         }
+//     }else{
+//         localStorage.setItem('name', e.target.innerText);
+//     }
+// }
+// console.log(setClientName.textContent);
+// // Get client message 
 
-function getClientMessage(){
-    if(localStorage.getItem('message') === null){
-        clientMessage.textContent = `${'[Your message].'}`;
-    } else {
-        clientMessage.textContent = localStorage.getItem('message');
-    }
-}
-// set client message
+// function getClientMessage(){
+//     if(localStorage.getItem('message') === null){
+//         clientMessage.textContent = `${'[Your message].'}`;
+//     } else {
+//         clientMessage.textContent = localStorage.getItem('message');
+//     }
+// }
+// // set client message
 
-function setClientMessage(e){
-    if(e.type === 'keypress'){
-        if(e.keyCode == 13){
-            localStorage.setItem('message', e.target.innerText);
-            clientMessage.blur();
-        }
-    }else{
-        localStorage.setItem('message', e.target.innerText);
-    }
-}
-console.log(setClientMessage.textContent);
-getClientName();
-getClientMessage();
-// setClientMessage();
-// setClientName();
+// function setClientMessage(e){
+//     if(e.type === 'keypress'){
+//         if(e.keyCode == 13){
+//             localStorage.setItem('message', e.target.innerText);
+//             clientMessage.blur();
+//         }
+//     }else{
+//         localStorage.setItem('message', e.target.innerText);
+//     }
+// }
+// console.log(setClientMessage.textContent);
+// getClientName();
+// getClientMessage();
+// // setClientMessage();
+// // setClientName();
 
-// Call function()'s before eventListeners
+// // Call function()'s before eventListeners
 
-// addEventListener
-clientName.addEventListener('keypress', setClientName);
-clientName.addEventListener('blur', setClientName);
-clientMessage.addEventListener('keypress', setClientMessage);
-clientMessage.addEventListener('blur', setClientMessage);
+// // addEventListener
+// clientName.addEventListener('keypress', setClientName);
+// clientName.addEventListener('blur', setClientName);
+// clientMessage.addEventListener('keypress', setClientMessage);
+// clientMessage.addEventListener('blur', setClientMessage);
 
--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,52 +1,52 @@
 // get form btn
 const registrationBtn = document.getElementById('registration-btn');
+// console.log();
 
-// get form
-const form = document.querySelectorAll('registration-form');
-
-// get input fields
-let firstName = document.getElementById('first-name');
-let lastName = document.getElementById('last-name');
-let email = document.getElementById('email');
-let passWord = document.getElementById('password');
-let confirmPassWord = document.getElementById('confirm-password');
 //  get err fields
 let elFirstName = document.getElementById('err-first-name');
 let elLastName = document.getElementById('err-last-name');
 let elEmail = document.getElementById('err-email');
 let elPassWord = document.getElementById('err-password');
 let elConfirmPassWord = document.getElementById('err-confirm-password');
-console.log(firstName.value);
-console.log(form.value);
+// console.log();
 
 
 
 // EventListener's
-registrationBtn.addEventListener('submit', submitRegistrationForm);
-
 
 
 // submit registration function
+
+registrationBtn.addEventListener('click', submitRegistrationForm);
+
 function submitRegistrationForm(e){
     e.preventDefault();
+    console.log('Testing Submit Register');
 
-const firstName = document.getElementById('first-name').value;
-const lastName = document.getElementById('last-name').value;
-const email = document.getElementById('email').value;
-const passWord = document.getElementById('password').value;
-const confirmPassWord = document.getElementById('confirm-password').value;
+    // get form
+    const form = document.getElementById('registration-form');
+    
+    // // get input fields
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const email = document.getElementById('email').value;
+    const passWord = document.getElementById('password').value;
+    const confirmPassWord = document.getElementById('confirm-password').value;
     const id = new Date().getTime();
     const user = {
-        id: id,
-        userName: userName,
-        userEmail: userEmail,
-        userImg: userImg,
-        userComment: userComment,
+        id,
+        firstName,
+        lastName,
+        email,
+        passWord,
+        confirmPassWord,
         done: true
     }
     
     const userRegistrationValues = JSON.parse(localStorage.getItem('userRegistrationValues')) || [];
     userRegistrationValues.push(user);
     localStorage.setItem('userRegistrationValues', JSON.stringify(userRegistrationValues));
-    this.reset();
-}
+    form.reset();
+    console.log(registrationBtn.target);
+  }
+  
