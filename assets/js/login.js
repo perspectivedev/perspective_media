274c506539c6f6fe7d5853f20926846a184f6680
= <=>
2022-12-16 21:35:15 -0800

    introduced the login modal. Still need to fix css

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,10 @@
+// Get modal 
+const loginModal = document.getElementById('login-modal');
+// Get open modal btn
+const openLoginModalBtn = document.getElementById('login-btn');
+// Get close modal btn
+const closeLoginModalBtn = document.getElementById('close-login-modal-btn');
+
 // eye-btn
 const visibilityBtn = document.getElementById('visibility');
 
@@ -5,6 +12,24 @@ const EYE_SHOW = String.fromCharCode(0xF070);
 const EYE_HIDE = String.fromCharCode(0xF06E);
 
 visibilityBtn.innerHTML = EYE_SHOW;
+
+
+// function's to open/close modal
+function openLoginModal() {
+  loginModal.style.display = 'block';
+}
+
+
+function closeLoginModal() {
+  loginModal.style.display = 'none';
+}
+
+
+function clickOutsideLogin(e) {
+  if (e.target === loginModal) {
+    loginModal.style.display = 'none';
+  }
+}
 
 //toggleVisibility()
 function passwordVisibility() {
@@ -20,4 +45,14 @@ function passwordVisibility() {
 }
 
 // eventlistener
+openLoginModalBtn.addEventListener('click', openLoginModal);
+closeLoginModalBtn.addEventListener('click', closeLoginModal);
+window.addEventListener('click', clickOutsideLogin);
 visibilityBtn.addEventListener('click', passwordVisibility);
+
+
+
+
+
+
+
